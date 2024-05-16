a66db1610c082717ec0f91d2977c51d4d39f966c
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-05-17 00:04:35 +0530

    1:27:00

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -6,22 +6,18 @@ import Card from "../../components/card/Card";
 function listPage()
 {
 const data=listData;
-
-    return (
-        <div className="listPage">
-        <div className="listContainer">
-        <div className="wrapper">
-        <Filter/>
-        {
-        data.map(item=>(
-        <Card key={item.id} data={item}/>
-        )
-        )}
-
-        </div>
-        </div>
-        <div className="mapContainer">Map</div>
-        </div>
-    )
+return <div className="listPage">
+<div className="listContainer">
+  <div className="wrapper">
+    <Filter/>
+    {data.map(item=>(
+      <Card key={item.id} item={item}/>
+    ))}
+  </div>
+</div>
+<div className="mapContainer">
+    Map
+</div>
+</div>;
 }
 export default listPage
